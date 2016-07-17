22d443bbab7cb7148121a533f4fa11b68069f272
br-monteiro <bruno.monteirodg@gmail.com>
2016-07-16 23:23:21 -0300

    removido comentário com o construtor

--- projeto_final/MyPizza/app/Http/Controllers/IndexController.php
+++ projeto_final/MyPizza/app/Http/Controllers/IndexController.php
@@ -8,17 +8,6 @@ use Illuminate\Support\Facades\Validator;
 
 class IndexController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     *
-    public function __construct()
-    {
-        //
-    }
-
-    /*/
     public function index()
     {
         return view('layout');
